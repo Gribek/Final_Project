a457dc4286dc8d34221fb124f08b3bfe8fe2a44f
Piotr Maslewski <piotrekmaslewski@gmail.com>
2018-12-19 14:09:56 +0100

    context_processor działa niezależnie od zalogowania

--- RunScheduleApp/context_processor.py
+++ RunScheduleApp/context_processor.py
@@ -1,9 +1,16 @@
 from RunScheduleApp.views import get_month_counter
 from RunScheduleApp.models import WorkoutPlan
 
+
 def pass_month_counter_to_base_html(request):
-    plan_start_date = WorkoutPlan.objects.filter(owner=request.user).filter(is_active=True)[0].date_range.lower
-    ctx = {
-        'get_month_counter': get_month_counter(plan_start_date)
-    }
+    if not request.user.is_anonymous:
+        if not WorkoutPlan.objects.filter(owner=request.user).filter(is_active=True).exists():
+            ctx = {'get_month_counter': 0}
+            return ctx
+        plan_start_date = WorkoutPlan.objects.filter(owner=request.user).filter(is_active=True)[0].date_range.lower
+        ctx = {
+            'get_month_counter': get_month_counter(plan_start_date)
+        }
+        return ctx
+    ctx = {'get_month_counter': 0}
     return ctx
