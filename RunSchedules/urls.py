fef8283ae5bd5d69bde16c83ba8875c8f434f165
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-03-29 16:46:31 +0100

    Removed redundant url pattern

--- RunSchedules/urls.py
+++ RunSchedules/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     url(r'^workout_list$', WorkoutsList.as_view()),
     url(r'^workout_plan_add/', WorkoutPlanAdd.as_view()),
     url(r'^workout_plan_edit/(?P<plan_id>\d+)$', WorkoutPlanEdit.as_view()),
-    # url(r'^current_workout_plan/', CurrentWorkoutPlanView.as_view()),
     url(r'^plan_details/(?P<id>\d+)$', PlanDetailsView.as_view()),
     url(r'^daily_training_add/(?P<id>\d+)$', DailyTrainingAdd.as_view()),
     url(r'^daily_training_add/(?P<id>\d+)/(?P<date>.+)$', DailyTrainingAdd.as_view()),
