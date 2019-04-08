67c10ba62adce09b6744e0692a5f40c73da83c4e
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-04-08 13:18:46 +0200

    Refactored test_views code.

--- RunScheduleApp/test_views.py
+++ RunScheduleApp/test_views.py
@@ -18,41 +18,6 @@ class MainPageViewTest(TestCase):
         self.assertTemplateUsed(response, 'RunScheduleApp/main_page.html')
 
 
-class WorkoutsListTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        test_user = User.objects.create_user(username='test_user', password='test')
-        test_user_2 = User.objects.create_user(username='test_user_2', password='test')
-        WorkoutPlan.objects.create(plan_name='plan name1', date_range=["2011-01-01", "2018-01-31"], owner=test_user)
-        WorkoutPlan.objects.create(plan_name='plan name2', date_range=["2011-01-01", "2018-01-31"], owner=test_user_2)
-        WorkoutPlan.objects.create(plan_name='plan name3', date_range=["2011-01-01", "2018-01-31"], owner=test_user)
-        WorkoutPlan.objects.create(plan_name='plan name4', date_range=["2011-01-01", "2018-01-31"], owner=test_user_2)
-        WorkoutPlan.objects.create(plan_name='plan name5', date_range=["2011-01-01", "2018-01-31"], owner=test_user)
-
-    def test_view_url_exist_at_desired_location(self):
-        self.client.login(username='test_user', password='test')
-        response = self.client.get('/workout_list')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_redirects_if_not_logged_in(self):
-        response = self.client.get('/workout_list')
-        self.assertRedirects(response, f'/login?next=/workout_list')
-
-    def test_view_uses_correct_template(self):
-        self.client.login(username='test_user', password='test')
-        response = self.client.get('/workout_list')
-        self.assertTemplateUsed(response, 'RunScheduleApp/workoutplan_list.html')
-
-    def test_view_returns_correct_workout_plans_list_in_context(self):
-        self.client.login(username='test_user', password='test')
-        response = self.client.get('/workout_list')
-        self.assertTrue('workout_plans' in response.context, 'Key not found in context dictionary')
-        self.assertEqual(len(response.context['workout_plans']), 3, 'Context does not contain all user plans')
-        other_user_workout_plan = WorkoutPlan.objects.get(plan_name='plan name2')
-        self.assertFalse(other_user_workout_plan in response.context['workout_plans'],
-                         'Context contain workout plan belonging to another user')
-
-
 class PermissionRequiredViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -62,12 +27,42 @@ class PermissionRequiredViewTest(TestCase):
                                    is_active=True)
         WorkoutPlan.objects.create(plan_name='setUp plan 2', date_range=["2011-01-01", "2018-01-31"], owner=user_2,
                                    is_active=False)
+        WorkoutPlan.objects.create(plan_name='setUp plan 3', date_range=["2011-01-01", "2018-01-31"], owner=user_1)
+        WorkoutPlan.objects.create(plan_name='setUp plan 4', date_range=["2011-01-01", "2018-01-31"], owner=user_1)
+        WorkoutPlan.objects.create(plan_name='setUp plan 5', date_range=["2011-01-01", "2018-01-31"], owner=user_2)
 
     def log_user_with_permission(self):
         self.client.login(username='user_with_permission', password='test')
 
     def log_non_permission_user(self):
         self.client.login(username='non_permission_user', password='test')
+
+
+class WorkoutsListTest(PermissionRequiredViewTest):
+    def test_view_url_exist_at_desired_location(self):
+        self.log_user_with_permission()
+        response = self.client.get('/workout_list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_redirects_if_not_logged_in(self):
+        response = self.client.get('/workout_list')
+        self.assertRedirects(response, f'/login?next=/workout_list')
+
+    def test_view_uses_correct_template(self):
+        self.log_user_with_permission()
+        response = self.client.get('/workout_list')
+        self.assertTemplateUsed(response, 'RunScheduleApp/workoutplan_list.html')
+
+    def test_view_returns_correct_workout_plans_list_in_context(self):
+        self.log_user_with_permission()
+        response = self.client.get('/workout_list')
+        self.assertTrue('workout_plans' in response.context, 'Key not found in context dictionary')
+        number_of_user_plans = len(WorkoutPlan.objects.filter(owner__username='user_with_permission'))
+        self.assertEqual(len(response.context['workout_plans']), number_of_user_plans,
+                         'Context does not contain all user plans')
+        other_user_workout_plan = WorkoutPlan.objects.get(plan_name='setUp plan 2')
+        self.assertFalse(other_user_workout_plan in response.context['workout_plans'],
+                         'Context contain workout plan belonging to another user')
 
 
 class PlanDetailsViewTest(PermissionRequiredViewTest):
@@ -96,8 +91,9 @@ class PlanDetailsViewTest(PermissionRequiredViewTest):
 
     def test_view_checks_if_user_is_owner_of_the_workout_plan(self):
         self.log_user_with_permission()
+        other_user_workout_plan = WorkoutPlan.objects.filter(owner__username='non_permission_user')[0]
         response = self.client.get(
-            f'/plan_details/{WorkoutPlan.objects.get(owner__username="non_permission_user").id}')
+            f'/plan_details/{other_user_workout_plan.id}')
         self.assertEqual(response.status_code, 403)
 
     def test_view_uses_correct_template(self):
