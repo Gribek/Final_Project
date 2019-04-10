3f65df348b3a3113afae27acf0fe85ef287ceead
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-04-10 12:23:57 +0200

    Fixed bug in context processor.

--- RunScheduleApp/context_processor.py
+++ RunScheduleApp/context_processor.py
@@ -4,10 +4,10 @@ from RunScheduleApp.models import WorkoutPlan
 
 def pass_month_counter_to_base_html(request):
     if request.user.is_anonymous:
-        ctx = {'get_month_counter': 0}
+        ctx = {'get_month_counter': 1}
         return ctx
     if not WorkoutPlan.objects.filter(owner=request.user).filter(is_active=True).exists():
-        ctx = {'get_month_counter': 0}
+        ctx = {'get_month_counter': 1}
         return ctx
     plan_start_date = WorkoutPlan.objects.filter(owner=request.user).filter(is_active=True)[0].date_range.lower
     ctx = {
