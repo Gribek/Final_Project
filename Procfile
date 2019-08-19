732fa0917e15c94023d53b6feabcc8334084ebf5
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-08-19 13:52:54 +0200

    Prepared to deploy to heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn RunSchedules.wsgi --log-file -
--- RunSchedules/settings.py
+++ RunSchedules/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,7 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_URL = '/login'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
+# ]
+
+# Heroku settings.
+django_heroku.settings(locals())
